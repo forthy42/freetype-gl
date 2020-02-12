09252b21890905581c52d6c3ed54b9ecf4c64791
Bernd Paysan <bernd.paysan@gmx.de>
2020-02-12 13:55:26 +0100

    Add library search paths for Darwin

--- texture-font.c
+++ texture-font.c
@@ -306,6 +306,7 @@ texture_font_init(texture_font_t *self)
     self->height = 0;
     self->ascender = 0;
     self->descender = 0;
+    self->linegap = 0;
     self->rendermode = RENDER_NORMAL;
     self->outline_thickness = 0.0;
     self->hinting = 1;
